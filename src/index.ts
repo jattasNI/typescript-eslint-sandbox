38181c45d7d3eb1dc143289bce1110e9311facd8
jattasNI <jattasNI@users.noreply.github.com>
2021-05-07 15:58:53 -0500

    Switch example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 const world = 'world';
+enum MyEnum { Foo='foo', Bar='bar'};
 
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
+export function hello(word: MyEnum): string {
+  switch (word) {
+      case MyEnum.Bar:
+          console.log('fall');
+          return 'stuff';
+      case MyEnum.Foo:
+          console.log('hello');
+          return 'hello';
+  };
+  return 'x';
 }
