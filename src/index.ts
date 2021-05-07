b70abb52d2181588364c740c9fe95cfb342db10e
jattasNI <jattasNI@users.noreply.github.com>
2021-05-07 16:35:28 -0500

    ni ruleset

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 const world = 'world';
-enum MyEnum { Foo='foo', Bar='bar'};
+enum MyEnum { Foo = 'foo', Bar = 'bar'}
 
 export function hello(word: MyEnum): string {
-  switch (word) {
-      case MyEnum.Bar:
-          console.log('fall');
-          return 'stuff';
-      case MyEnum.Foo:
-          console.log('hello');
-          return 'hello';
-  };
-  return 'x';
+    switch (word) {
+        case MyEnum.Bar:
+            console.log('fall');
+            return 'stuff';
+        case MyEnum.Foo:
+            console.log('hello');
+            return 'hello';
+    }
+    return 'x';
+}
+
+export class MyClass {
+    foo: string;
+    /**
+     *
+     */
+    constructor() {
+        this.foo = 'hi';
+    }
 }
